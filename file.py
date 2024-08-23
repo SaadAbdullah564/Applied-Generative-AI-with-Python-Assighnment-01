642f1b6e89a841f31f97fef8ce23d09e3e323c3d
Saad Abdullah <saadabdullah0066@gmail.com>
2024-08-23 17:37:54 +0500

    init git course name

---
+++ file.py
@@ -0,0 +1,16 @@
+#let us have a cylinder of unknown volume 
+#now we can write a python program to find the volume of cylinder
+heightCylinder = 5.5
+radiusCylinder = 3.5
+pie = 3.1416
+volume = pie * radiusCylinder**2 * heightCylinder 
+print(volume)
+
+
+#now we can wrte a python program to find the BMI of a person
+weightPerson = 80 
+heightPerson = 1.5 
+secondHeight = heightPerson * heightPerson
+print(secondHeight)
+bmiPerson = weightPerson / secondHeight
+print( "Your BMI is " , bmiPerson) 
---
+++ homework.py
@@ -0,0 +1,49 @@
+#let a rectangle whose area is unknown
+length = 5
+width = 10 
+area = length * width 
+print(area)
+
+
+#let us make a program for finding our current Age
+birthYear = 2007
+currentYear = 2024
+currentAge = currentYear - birthYear 
+print(currentAge)
+
+
+#let us have two variables of known values
+a = 120
+inMinutes = a / 60
+print(inMinutes)
+inSeconds = 120 * 60
+print(inSeconds) 
+
+
+#write a python program  in we can convert temprature from celsius to farenhiet scale and vice versa
+celsiusTemprature = 20
+farenhietTemprature = (celsiusTemprature * 9/5 ) + 32
+print(farenhietTemprature)
+farenhietTemp = 45 
+celsiusTemp = (farenhietTemp  * 5/9 ) -32
+print(celsiusTemp)
+
+
+#let us have a cube of unknown area we need to find its area from a python program
+lenghtSide = 5
+formula = 6 * (lenghtSide) ** 2
+print(formula)
+
+
+#now we have a circle of unknown area so we may find its area with the help of a python programm
+radiusCircle = 2.5 
+pie = 3.1416
+area = pie * (radiusCircle)**2
+print(area)
+
+
+#write a python programm to find the percentage of a stydent
+totalMarks = 570
+obtainedMarks = 517
+percentage = (obtainedMarks / totalMarks ) * 100
+print("Your percentage is " , percentage , "%")
